233bda277844e9133e58aac52fb2be9ae9409246
alain burindi <alainburindi62@gmail.com>
2019-07-29 17:42:17 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isDevelopment = process.env.NODE_ENV === "development";
+
+module.exports = {
+  devtool: "inline-source-map",
+  entry: "./src/index.js",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build/"),
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  devServer: { contentBase: "./build" },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, use: ["babel-loader"] },
+      {
+        test: /\.module\.s(a|c)ss$/,
+        loader: [
+          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              localIdentName: "[name]__[local]___[hash:base64:5]",
+              camelCase: true,
+              sourceMap: isDevelopment
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: isDevelopment
+            }
+          }
+        ]
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        exclude: /\.module.(s(a|c)ss)$/,
+        loader: [
+          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: isDevelopment
+            }
+          }
+        ]
+      },
+      {
+        test: /.(jpg|jpeg|png|gif|svg)$/,
+        use: ["file-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve("./public/index.html")
+    }),
+    new MiniCssExtractPlugin({
+      filename: isDevelopment ? "[name].css" : "[name].[hash].css",
+      chunkFilename: isDevelopment ? "[id].css" : "[id].[hash].css"
+    })
+  ],
+  resolve: {
+    extensions: [".js", ".jsx", ".scss"]
+  }
+};
