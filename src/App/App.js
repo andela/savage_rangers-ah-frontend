7f6efd612993c55f2963bb08e1761e7b4e4dcb13
alain burindi <alainburindi62@gmail.com>
2019-07-29 17:42:17 +0200

    initial commit

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Authors Haven</h1>
+        <button className="btn btn-primary daniel">yes</button>
+        <button className="btn daniel">jjjj</button>
+
+      </div>
+    );
+  }
+}
+
+export default App;
