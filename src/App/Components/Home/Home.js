7a41de4373f5f11c5953e7e0aaec64f9279072da
nkpremices <premices.tuvere@gmail.com>
2019-07-31 12:53:52 +0200

    [Ch #166240774] Setup react router v4

---
+++ src/App/Components/Home/Home.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import axiosInstance from '../../../../configs/axios';
+export class Home extends Component {
+  state = {
+    up: false,
+    down: false
+  }
+  async componentDidMount() {
+    const res = await axiosInstance.get('');
+    if (res) this.setState({ up: true })
+    else this.setState({ down: true })
+  }
+  render() {
+    if (this.state.up) {
+      return (
+        <React.Fragment>
+          <h1>Authors Heaven</h1>
+          <p>This is the home page of authors heaven v 1.0.0</p>
+        </React.Fragment>
+      )
+    } else if (this.state.down) {
+      return (
+        <React.Fragment>
+          <h1>Sorry an error occurred</h1>
+        </React.Fragment>
+      )
+    } else {
+      setTimeout(() => {
+        this.setState({ down: true })
+      }, 2000)
+      return (
+        <React.Fragment>
+          <h1>Animation</h1>
+        </React.Fragment>
+      )
+
+    }
+  }
+}
+
+export default Home
+
+
---
+++ src/App/Components/Login/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <React.Fragment>
+      <h1>Authors Heaven/Login</h1>
+      <p>Thi is the login page</p>
+    </React.Fragment>
+  )
+}
