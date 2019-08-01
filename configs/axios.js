6c727d4b91409a03e6db2887f0914e78c2eb2c88
nkpremices <premices.tuvere@gmail.com>
2019-08-01 15:09:57 +0200

    [Ft #166240778] Allow users to reset their passwords

---
+++ configs/axios.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+let instance;
+if (process.env.NODE_ENV !== 'test') {
+  instance = axios.create({ baseURL: 'https://authors-haven-staging.herokuapp.com' });
+} else {
+  instance = axios;
+}
+export default instance;
