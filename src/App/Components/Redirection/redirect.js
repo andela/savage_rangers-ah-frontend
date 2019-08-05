57bf73e7ec7ddcfa05a6334da2125f9876773138
Frank Mutabazi <MCFrank16>
2019-08-05 21:14:41 +0200

    [ft -#166240781] adding social login frontend logic

---
+++ src/App/Components/Redirection/redirect.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import queryString from 'query-string';
+
+
+export class redirect extends Component {
+  componentDidMount() {
+    const { token } = queryString.parse(window.location.search);
+    localStorage.setItem('token', token);
+  }
+
+  render() {
+    return (
+      <div>
+        <center id="redirect-text">
+          Redirection Page
+        </center>
+      </div>
+    );
+  }
+}
+
+export default redirect;
